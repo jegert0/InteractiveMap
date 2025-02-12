c6b60c8e96f29a74766ed48bff3d3b7b2e24beb4
jegert0 <jegertm@gmail.com>
2025-02-11 22:40:47 -0500

    first commit

---
+++ Assets/CesiumSettings/CesiumScripts/API.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CesiumForUnity;
+using Unity.VisualScripting;
+
+public class API : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public GameObject Ces;
+    public string Key = "";
+    
+    void Update(){
+        checkURL();
+    }
+    void checkURL(){
+        if (Key != ""){
+            var Source = Ces.GetComponent<Cesium3DTileset> ();
+            Source.tilesetSource = CesiumDataSource.FromUrl;
+            Source.url = "https://tile.googleapis.com/v1/3dtiles/root.json?key=" + Key;
+            
+
+        }
+    }
+}
